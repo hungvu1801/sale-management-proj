c3d17d5d596092436f4783c79422f5da9949473b
Mike <vutienhung2212@gmail.com>
2026-06-09 09:32:49 +0700

    [feat]: add form doi mat khau

--- Forms/frmDoiMatKhau.Designer.cs
+++ Forms/frmDoiMatKhau.Designer.cs
@@ -28,33 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDoiMK = new System.Windows.Forms.Button();
+            this.txtMKC = new System.Windows.Forms.TextBox();
+            this.txtMKM = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMKM);
+            this.groupBox1.Controls.Add(this.txtMKC);
+            this.groupBox1.Controls.Add(this.btnDoiMK);
+            this.groupBox1.Location = new System.Drawing.Point(33, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(347, 186);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnDoiMK
+            // 
+            this.btnDoiMK.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnDoiMK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDoiMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoiMK.ForeColor = System.Drawing.Color.White;
+            this.btnDoiMK.Location = new System.Drawing.Point(116, 127);
+            this.btnDoiMK.Name = "btnDoiMK";
+            this.btnDoiMK.Size = new System.Drawing.Size(173, 34);
+            this.btnDoiMK.TabIndex = 2;
+            this.btnDoiMK.Text = "Đối Mật Khẩu";
+            this.btnDoiMK.UseVisualStyleBackColor = false;
+            this.btnDoiMK.Click += new System.EventHandler(this.btnDoiMK_Click);
+            // 
+            // txtMKC
+            // 
+            this.txtMKC.Location = new System.Drawing.Point(116, 48);
+            this.txtMKC.Name = "txtMKC";
+            this.txtMKC.Size = new System.Drawing.Size(200, 20);
+            this.txtMKC.TabIndex = 3;
+            // 
+            // txtMKM
+            // 
+            this.txtMKM.Location = new System.Drawing.Point(116, 89);
+            this.txtMKM.Name = "txtMKM";
+            this.txtMKM.Size = new System.Drawing.Size(200, 20);
+            this.txtMKM.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(80, 35);
+            this.label1.Location = new System.Drawing.Point(33, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Đổi Mật Khẩu";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Mật Khẩu Cũ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Mật Khẩu Mới";
             // 
             // frmDoiMatKhau
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(411, 249);
+            this.Controls.Add(this.groupBox1);
             this.Name = "frmDoiMatKhau";
-            this.Text = "frmDoiMatKhau";
+            this.Text = "Đổi Mật Khẩu";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDoiMK;
+        private System.Windows.Forms.TextBox txtMKM;
+        private System.Windows.Forms.TextBox txtMKC;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
     }
 }
--- Forms/frmDoiMatKhau.cs
+++ Forms/frmDoiMatKhau.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QuanLyBanHang.Models;
+using QuanLyBanHang.Singletons;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,46 @@ namespace QuanLyBanHang.Forms
         public frmDoiMatKhau()
         {
             InitializeComponent();
+        }
+        private void ClearForm()
+        {
+            txtMKC.Text = string.Empty;
+            txtMKM.Text = string.Empty;
+        }
+        private void btnDoiMK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMKC.Text))
+            {
+                Utils.ShowMessage("Vui lòng nhập Mật Khẩu Cũ!", txtMKC);
+                return;
+            }
+            if (string.IsNullOrEmpty(txtMKM.Text)) 
+            {
+                Utils.ShowMessage("Vui lòng nhập Mật Khẩu Mới!", txtMKM);
+                return;
+            }
+            if (CurrentUsr.Instance.NhanVien.Password == txtMKC.Text.Trim()) 
+            {
+                CurrentUsr.Instance.NhanVien.Password = txtMKC.Text.Trim();
+                using (DBContextModel dbContext = new DBContextModel())
+                {
+                    NhanVien nv = dbContext.NhanViens
+                        .FirstOrDefault(n => n.MaNV == CurrentUsr.Instance.NhanVien.MaNV);
+
+                    if (nv == null) return;
+
+                    nv.Password = txtMKM.Text.Trim();
+                    dbContext.SaveChanges();
+
+                    // Update cho singleton
+                    CurrentUsr.Instance.NhanVien.Password = txtMKM.Text.Trim();
+
+                    Utils.ShowMessage("Đổi mật khẩu thành công!", txtMKM);
+                    ClearForm();
+                }
+                    
+            }
+            else Utils.ShowMessage("Mật khẩu cũ không đúng!", txtMKC);
         }
     }
 }

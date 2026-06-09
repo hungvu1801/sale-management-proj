1c37ccf9d0db619870f7522f9d0d1d101b282e43
NguyenThuyComputer <vutienhung2212@gmail.com>
2026-06-09 07:46:59 +0700

    [feat]: add quanlynv

--- Forms/frmNhanVien.Designer.cs
+++ Forms/frmNhanVien.Designer.cs
@@ -28,47 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dgvHangSX = new System.Windows.Forms.DataGridView();
+            this.dgvNV = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnXoa = new System.Windows.Forms.Button();
             this.btnSua = new System.Windows.Forms.Button();
             this.btnThem = new System.Windows.Forms.Button();
-            this.txtTenHangSX = new System.Windows.Forms.TextBox();
-            this.txtMaHangSX = new System.Windows.Forms.TextBox();
+            this.txtTenNV = new System.Windows.Forms.TextBox();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbAdmin = new System.Windows.Forms.RadioButton();
+            this.rbUser = new System.Windows.Forms.RadioButton();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvHangSX)).BeginInit();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNV)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dgvHangSX
+            // dgvNV
             // 
-            this.dgvHangSX.AllowUserToAddRows = false;
-            this.dgvHangSX.AllowUserToDeleteRows = false;
-            this.dgvHangSX.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHangSX.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvNV.AllowUserToAddRows = false;
+            this.dgvNV.AllowUserToDeleteRows = false;
+            this.dgvNV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
-            this.Column2});
-            this.dgvHangSX.Location = new System.Drawing.Point(185, 203);
-            this.dgvHangSX.Name = "dgvHangSX";
-            this.dgvHangSX.ReadOnly = true;
-            this.dgvHangSX.Size = new System.Drawing.Size(440, 290);
-            this.dgvHangSX.TabIndex = 5;
+            this.Column2,
+            this.Column3});
+            this.dgvNV.Location = new System.Drawing.Point(185, 231);
+            this.dgvNV.Name = "dgvNV";
+            this.dgvNV.ReadOnly = true;
+            this.dgvNV.Size = new System.Drawing.Size(440, 290);
+            this.dgvNV.TabIndex = 5;
+            this.dgvNV.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHangSX_CellClick);
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.rbUser);
+            this.groupBox1.Controls.Add(this.rbAdmin);
             this.groupBox1.Controls.Add(this.btnXoa);
             this.groupBox1.Controls.Add(this.btnSua);
             this.groupBox1.Controls.Add(this.btnThem);
-            this.groupBox1.Controls.Add(this.txtTenHangSX);
-            this.groupBox1.Controls.Add(this.txtMaHangSX);
+            this.groupBox1.Controls.Add(this.txtTenNV);
+            this.groupBox1.Controls.Add(this.txtMaNV);
             this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(185, 20);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(431, 162);
+            this.groupBox1.Size = new System.Drawing.Size(440, 191);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông Tin Nhân Viên";
@@ -78,50 +87,53 @@
             this.btnXoa.BackColor = System.Drawing.Color.Red;
             this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnXoa.ForeColor = System.Drawing.Color.White;
-            this.btnXoa.Location = new System.Drawing.Point(303, 104);
+            this.btnXoa.Location = new System.Drawing.Point(306, 135);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(100, 35);
             this.btnXoa.TabIndex = 16;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnSua
             // 
             this.btnSua.BackColor = System.Drawing.Color.Orange;
             this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSua.ForeColor = System.Drawing.Color.White;
-            this.btnSua.Location = new System.Drawing.Point(166, 104);
+            this.btnSua.Location = new System.Drawing.Point(169, 135);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(100, 35);
             this.btnSua.TabIndex = 13;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = false;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnThem
             // 
             this.btnThem.BackColor = System.Drawing.Color.LimeGreen;
             this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThem.ForeColor = System.Drawing.Color.White;
-            this.btnThem.Location = new System.Drawing.Point(28, 104);
+            this.btnThem.Location = new System.Drawing.Point(31, 135);
             this.btnThem.Name = "btnThem";
             this.btnThem.Size = new System.Drawing.Size(100, 35);
             this.btnThem.TabIndex = 15;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
-            // txtTenHangSX
+            // txtTenNV
             // 
-            this.txtTenHangSX.Location = new System.Drawing.Point(130, 63);
-            this.txtTenHangSX.Name = "txtTenHangSX";
-            this.txtTenHangSX.Size = new System.Drawing.Size(295, 20);
-            this.txtTenHangSX.TabIndex = 1;
+            this.txtTenNV.Location = new System.Drawing.Point(130, 63);
+            this.txtTenNV.Name = "txtTenNV";
+            this.txtTenNV.Size = new System.Drawing.Size(295, 20);
+            this.txtTenNV.TabIndex = 1;
             // 
-            // txtMaHangSX
+            // txtMaNV
             // 
-            this.txtMaHangSX.Location = new System.Drawing.Point(130, 26);
-            this.txtMaHangSX.Name = "txtMaHangSX";
-            this.txtMaHangSX.Size = new System.Drawing.Size(295, 20);
-            this.txtMaHangSX.TabIndex = 0;
+            this.txtMaNV.Location = new System.Drawing.Point(130, 26);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(295, 20);
+            this.txtMaNV.TabIndex = 0;
             // 
             // label4
             // 
@@ -141,6 +153,37 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên Nhân Viên";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Roles";
+            // 
+            // rbAdmin
+            // 
+            this.rbAdmin.AutoSize = true;
+            this.rbAdmin.Location = new System.Drawing.Point(130, 94);
+            this.rbAdmin.Name = "rbAdmin";
+            this.rbAdmin.Size = new System.Drawing.Size(54, 17);
+            this.rbAdmin.TabIndex = 17;
+            this.rbAdmin.TabStop = true;
+            this.rbAdmin.Text = "Admin";
+            this.rbAdmin.UseVisualStyleBackColor = true;
+            // 
+            // rbUser
+            // 
+            this.rbUser.AutoSize = true;
+            this.rbUser.Location = new System.Drawing.Point(245, 94);
+            this.rbUser.Name = "rbUser";
+            this.rbUser.Size = new System.Drawing.Size(47, 17);
+            this.rbUser.TabIndex = 17;
+            this.rbUser.TabStop = true;
+            this.rbUser.Text = "User";
+            this.rbUser.UseVisualStyleBackColor = true;
+            // 
             // Column1
             // 
             this.Column1.HeaderText = "Mã Nhân Viên";
@@ -155,16 +198,24 @@
             this.Column2.ReadOnly = true;
             this.Column2.Width = 200;
             // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Vai trò";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
             // frmNhanVien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(853, 545);
-            this.Controls.Add(this.dgvHangSX);
+            this.Controls.Add(this.dgvNV);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmNhanVien";
             this.Text = "Quản Lý Thông Tin Nhân Viên";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvHangSX)).EndInit();
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmNhanVien_FormClosed);
+            this.Load += new System.EventHandler(this.frmNhanVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNV)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -173,16 +224,20 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dgvHangSX;
+        private System.Windows.Forms.DataGridView dgvNV;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnXoa;
         private System.Windows.Forms.Button btnSua;
         private System.Windows.Forms.Button btnThem;
-        private System.Windows.Forms.TextBox txtTenHangSX;
-        private System.Windows.Forms.TextBox txtMaHangSX;
+        private System.Windows.Forms.TextBox txtTenNV;
+        private System.Windows.Forms.TextBox txtMaNV;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbUser;
+        private System.Windows.Forms.RadioButton rbAdmin;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
--- Forms/frmNhanVien.cs
+++ Forms/frmNhanVien.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using QuanLyBanHang.Models;
+using QuanLyBanHang.Singletons;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,176 @@ namespace QuanLyBanHang.Forms
 {
     public partial class frmNhanVien : Form
     {
+        private DBContextModel dbContext;
+
+        private List<NhanVien> lstNV;
+
+        private NhanVien selectedNV = null;
+
         public frmNhanVien()
         {
             InitializeComponent();
         }
+        private void PhanQuyen()
+        {
+            bool isAdmin = CurrentUsr.Instance.NhanVien.Role;
+            btnXoa.Visible = isAdmin;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (!ValidateData()) return;
+            if (ValidateIsExist())
+            {
+                Utils.ShowMessage("Nhân Viên đã tồn tại", txtTenNV);
+                return;
+            }
+
+            Insert();
+            ClearForm();
+            BindingData();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (!ValidateData()) return;
+            if (!ValidateIsExist())
+            {
+                Utils.ShowMessage("Nhân Viên chưa tồn tại", txtTenNV);
+                return;
+            }
+            Edit();
+            ClearForm();
+            BindingData();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            Delete();
+            selectedNV = null;
+            ClearForm();
+            BindingData();
+        }
+        private void dgvHangSX_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                DataGridViewRow row = dgvNV.Rows[e.RowIndex];
+                string maNV = row.Cells[0].Value.ToString();
+
+                selectedNV = lstNV.FirstOrDefault(s => s.MaNV == maNV); // not query directly to db
+                PopulateTxtBox();
+            }
+        }
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            dbContext = new DBContextModel();
+            BindingData();
+            PhanQuyen();
+        }
+
+        private void frmNhanVien_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            dbContext?.Dispose();
+        }
+
+        private void BindingData()
+        {
+            dgvNV.Rows.Clear();
+            lstNV = dbContext.NhanViens.ToList();
+            foreach (var sp in lstNV)
+            {
+                int idx = dgvNV.Rows.Add();
+                dgvNV.Rows[idx].Cells[0].Value = sp.MaNV;
+                dgvNV.Rows[idx].Cells[1].Value = sp.TenNV;
+                dgvNV.Rows[idx].Cells[2].Value = sp.Role? "Admin" : "User" ;
+            }
+
+        }
+
+        private void Insert()
+        {
+            string newMaNCC = Utils.GenNewNVID(dbContext);
+            string tenNCC = txtTenNV.Text;
+            string defaulPassword = "123";
+            NhanVien newNCC = new NhanVien
+            {
+                MaNV = newMaNCC,
+                TenNV = tenNCC,
+                Role = true,
+                Password = defaulPassword,
+            };
+            dbContext.NhanViens.Add(newNCC);
+            dbContext.SaveChanges();
+            Utils.ShowMessage("Thêm Nhân Viên Thành Công.", txtMaNV);
+        }
+
+        private void Delete()
+        {
+            try
+            {
+                if (selectedNV == null)
+                {
+                    Utils.ShowMessage("Vui lòng chọn dữ liệu để xóa!", txtMaNV);
+                    return;
+                }
+
+                var result = MessageBox.Show("Bạn có chắc chắn muốn xóa Nhân Viên này? Các Thông tin về Phiếu Nhập Có thể sẽ bị xóa theo", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result != DialogResult.Yes)
+                    return;
+                
+                dbContext.NhanViens.Remove(selectedNV);
+                dbContext.SaveChanges();
+                Utils.ShowMessage("Xóa Nhân Viên Thành Công.", txtMaNV);
+            }
+            catch (Exception ex)
+            {
+
+                Utils.ShowMessage("Lỗi xóa dữ liệu: " + ex.Message, txtMaNV);
+            }
+        }
+
+        private void Edit()
+        {
+            selectedNV.TenNV = txtTenNV.Text;
+            dbContext.SaveChanges();
+            Utils.ShowMessage("Chỉnh sửa thông tin Nhân Viên Thành Công.", txtTenNV);
+        }
+
+        private bool ValidateData()
+        {
+            if (string.IsNullOrWhiteSpace(txtMaNV.Text))
+            {
+                Utils.ShowMessage("Vui lòng điền tên Nhân Viên!", txtMaNV);
+                return false;
+            }
+            if (string.IsNullOrWhiteSpace(txtTenNV.Text))
+            {
+                Utils.ShowMessage("Vui lòng điền Số Điện Thoại Nhân Viên!", txtTenNV);
+                return false;
+            }
+           
+            return true;
+        }
+
+        private bool ValidateIsExist()
+        {
+            return selectedNV != null;
+        }
+        private void ClearForm()
+        {
+            txtMaNV.Text = "";
+            txtTenNV.Text = "";
+        }
+
+        private void PopulateTxtBox()
+        {
+            if (selectedNV == null) return;
+            txtMaNV.Text = selectedNV.MaNV.ToString();
+            txtTenNV.Text = selectedNV.TenNV.ToString();
+            rbAdmin.Checked = selectedNV.Role;
+            rbUser.Checked = !selectedNV.Role;
+        }
     }
 }
+
